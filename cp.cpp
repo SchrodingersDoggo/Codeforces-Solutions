92c112be0d57ea2e040462f6e33092e9c1a48899
Schrodinger's Doggo <alammdafzal29@gmail.com>
2026-05-17 15:50:23 +0530

    1744C

---
+++ cp.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+const char nl = '\n';
+
+void fastio(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+
+void solve(){
+   
+}
+
+
+
+
+
+
+int main(){
+    fastio();
+
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
